d226237788e074aac8fea98aeed80f0765205e22
Zamith <zamith.28@gmail.com>
2013-07-22 22:19:38 +0100

    My first jruby on rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Testapp::Application.config.secret_key_base = '767d0c6003debc0f78e0f36a5f2c38c30b43926eff6c298c6fea04767f1f4cc8ac94aa68bd514c180b081bdf06b5cc90926c7f4755f66092f6dd9f6942e9a510'
